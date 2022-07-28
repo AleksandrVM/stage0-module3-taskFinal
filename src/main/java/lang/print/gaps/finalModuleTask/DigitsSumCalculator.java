df046006f64c7375e5d634e96d9e6560431d3d14
Sasha-PC\Sasha <aleksandr.v.g.m@gmail.com>
2022-07-28 16:46:31 +0300

    solution FINAL MODULE TASK

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int first = number/1000;
+        int second = number/100 - first*10;
+        int third = number/10 - first*100 - second*10;
+        int forth = number - first*1000 - second*100 - third*10;
+        int sum = first + second + third + forth;
+        System.out.println(sum);
     }
 }
